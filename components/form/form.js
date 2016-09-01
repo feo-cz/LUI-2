4cc16b3c5ba116053344feb51dd0d872395568a8
Jiří Bělský <jiri.belsky@4every1.cz>
2016-09-01 11:30:22 +0200

    jQuery datepicker added

--- components/form/form.js
+++ components/form/form.js
@@ -7,6 +7,34 @@ $(document).ready(function () {
 		$(this).selectmenu({appendTo: dropdown});
 	});
 
+	$("input.make_lui_datepicker").each(function () {
+		if($(this).attr("data-format")){
+			var format = $(this).attr("data-format");
+		}
+		else {
+			var format = "yy-mm-dd";
+		}
+
+		$(this).datepicker({
+			dateFormat: format
+		});
+	});
+
+	$("input.make_lui_datepicker_select").each(function () {
+		if($(this).attr("data-format")){
+			var format = $(this).attr("data-format");
+		}
+		else {
+			var format = "yy-mm-dd";
+		}
+
+		$(this).datepicker({
+			dateFormat: format,
+			changeMonth: true,
+			changeYear: true
+		});
+	});
+
 	// FORM PREFIX / POSTFIX
 	/*$(window).load(function () {
 		function prefix_calc() {
